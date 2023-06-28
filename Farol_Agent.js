09b72ec5046fb4ecba6a03efca3d284e49ae8daf
M-Gentes <manuel.gentes@gmail.com>
2023-06-28 21:31:54 +0200

    adjust color map for more distinctive colors

--- Farol_Agent.js
+++ Farol_Agent.js
@@ -102,13 +102,13 @@ function setupColorMap() {
 
     for(let i = 0; i < color_map_length;) {
         if(i<10){
-            colorMap[i] = `rgb(${100-10*i}%, 0%, 0%)`;
-            colorMap[i+1] = `rgb(0%, ${100-10*i}%, 0%)`;
-            colorMap[i+2] = `rgb(0%, 0%, ${100-10*i}%)`;
+            colorMap[i] = `rgb(${100-20*Math.floor(i/3)}%, 0%, 0%)`;
+            colorMap[i+1] = `rgb(0%, ${100-20*Math.floor(i/3)}%, 0%)`;
+            colorMap[i+2] = `rgb(0%, 0%, ${100-20*Math.floor(i/3)}%)`;
         } else {
-            colorMap[i] = `rgb(100%, ${i}%, ${i}%)`;
-            colorMap[i+1] = `rgb(${i}%, 100%, ${i})%`;
-            colorMap[i+2] = `rgb(${i}%, ${i}%, 100%)`;
+            colorMap[i] = `rgb(100%, ${(i*i)%100}%, ${(10*i)%100}%)`;
+            colorMap[i+1] = `rgb(${(10*i)%100}%, 100%, ${(i*i)%100}%)`;
+            colorMap[i+2] = `rgb(${(i*i)%100}%, ${(10*i)%100}%, 100%)`;
         }
         i = i + 3;
     }
