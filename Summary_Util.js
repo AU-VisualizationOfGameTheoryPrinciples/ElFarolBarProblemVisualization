2137a9ce5f6c399aa70710592c0546a93af6dbb3
M-Gentes <manuel.gentes@gmail.com>
2023-07-07 01:03:19 +0200

    fix bug

--- Summary_Util.js
+++ Summary_Util.js
@@ -1,8 +1,8 @@
 function showSummary(id, countGoodDays, countBadDays, iterations, elementToUse = null) {
     var summary_elem = document.getElementById(id);
     var summary_text = elementToUse ? elementToUse : document.createElement("p");
-    let badDays_plural = countBadDays > 1 ? "s" : "";
-    let goodDays_plural = countGoodDays > 1 ? "s" : "";
+    let badDays_plural = countBadDays == 1 ? "" : "s";
+    let goodDays_plural = countGoodDays == 1 ? "" : "s";
     summary_text.innerText = countGoodDays + ` good day${goodDays_plural} and ` + countBadDays + ` bad day${badDays_plural} out of ` + iterations + " days watched (" + Number.parseInt(countGoodDays/iterations*100) + "%).";
     summary_elem.append(summary_text);
 }
